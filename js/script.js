dcc278ec4fb9d8a20c9e86858dcbd8e1e3858f0f
prayudaprianggara <prayudaprianggara@gmail.com>
2019-12-07 15:33:52 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,42 +1,30 @@
-//event pada saat diklik
-/*$('.page-scroll').on('click',function () {
-    //ambil isi href
-    var tujuan = $(this).attr('href');
-    //tangkap elemen yang bersangkuta
-    var elementTujuan = $(tujuan);
-    //pindah scroll
-    $('body').animate({
-        scrollTop: elementTujuan.offset().top - 50
-    }, 1000,'swing');
-});*/
-$(".page-scroll").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-        event.preventDefault();
-
-      // Store hash
-        var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    //event saat diklik
+    $('.page-scroll').on('click', function(e){
+        //ambil isi href
+        var tujuan = $(this).attr('href');
+        //tangkap elemen ybs
+        var elemenTujuan = $(tujuan);
+        //pindah scroll
         $('html, body').animate({
-        scrollTop: $(hash).offset().top
-        }, 800, 'swing', function(){
+            scrollTop: elemenTujuan.offset().top -50
+        },1000,'easeInOutExpo');
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-        });
-    } // End if
+        e.preventDefault();
     });
 
-    //parallax
-    $(window).scrollTop(function () {
-        wScroll = $(this).scrollTop();
 
-        /*$('.jumbotron img').css({
-            'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    //parallax
+    //about
+    $(window).on('load', function(){
+        $('.pKiri').addClass('pMuncul');
+        $('.pKanan').addClass('pMuncul');
+    });
+
+    $(window).scroll(function(){
+        var wScroll = $(this).scrollTop();
+        //jumbotron
+        $('.jumbotron img').css({
+            'transform' : 'translate(0px, '+ wScroll/8 +'%)'
         });
 
         $('.jumbotron h1').css({
@@ -44,17 +32,15 @@ $(".page-scroll").on('click', function(event) {
         });
 
         $('.jumbotron p').css({
-            'transform' : 'translate(0px, '+ wScroll +'%)'
-        });*/
+            'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+        });
 
         //portfolio
-        if(wScroll > $('.portfolio').offset().top - 250){
-            $('.portfolio .thumbnail').each(function(){
+        if(wScroll > $('.portfolio').offset(). top - 230) {
+            $('.portfolio .thumbnail').each(function(i){
                 setTimeout(function(){
-                    console.log('ok');
-                }, 2000);
+                    $('.portfolio .thumbnail').eq(i).addClass('muncul');
+                },300 * (i+1));
             });
-            
-            //$('.portfolio .thumbnail').addClass('muncul')
         }
     });
